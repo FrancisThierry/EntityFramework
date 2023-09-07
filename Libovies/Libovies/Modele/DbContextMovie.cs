288286b52d32c38dcdeb4ece708e3eb7a5d2d1b4
FrancisThierry <boulangerthie@gmail.com>
2023-09-07 19:42:42 +0200

    Exemple web avec EF CRUD

---
+++ Libovies/Libovies/Modele/DbContextMovie.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libovies.Modele
+{
+    public class DbContextMovie : DbContext
+    {
+        string DbPath = @"C:\projets\TDEF\databaseTD1.db";
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source = {DbPath}");
+        }
+
+    }
+
+
+}
+
